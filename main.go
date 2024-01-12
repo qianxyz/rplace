062c2b26d4295937ce3449f0ab260017566d9405
Qianxy <xyqian67@gmail.com>
2024-01-11 17:48:20 -0800

    Add toggle

--- main.go
+++ main.go
@@ -19,8 +19,7 @@ func getPixel(c *gin.Context) {
 		Y int `form:"y"`
 	}
 
-	err := c.Bind(&pos)
-	if err != nil {
+	if err := c.Bind(&pos); err != nil {
 		return
 	}
 
@@ -28,12 +27,30 @@ func getPixel(c *gin.Context) {
 	// TODO: Handle OOB
 	v := (grid[i/8] >> (7 - i%8)) & 1
 
-	c.JSON(http.StatusOK, v)
+	c.String(http.StatusOK, "%b", v)
+}
+
+func togglePixel(c *gin.Context) {
+	var pos struct {
+		X int `json:"x"`
+		Y int `json:"y"`
+	}
+
+	if err := c.Bind(&pos); err != nil {
+		return
+	}
+
+	i := pos.X*width + pos.Y
+	// TODO: Handle OOB
+	grid[i/8] ^= (1 << (7 - i%8))
+
+	c.String(http.StatusOK, "")
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/pixel", getPixel)
+	router.POST("/toggle", togglePixel)
 
 	router.Run(":8080")
 }

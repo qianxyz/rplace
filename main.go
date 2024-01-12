ce7d4ff736899216126510714ba0af413e2bd12b
Qianxy <xyqian67@gmail.com>
2024-01-11 17:28:24 -0800

    Handle getting single pixel

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+const (
+	width  = 16
+	height = 16
+)
+
+var grid = [width * height / 8]byte{}
+
+func getPixel(c *gin.Context) {
+	var pos struct {
+		X int `form:"x"`
+		Y int `form:"y"`
+	}
+
+	err := c.Bind(&pos)
+	if err != nil {
+		return
+	}
+
+	i := pos.X*width + pos.Y
+	// TODO: Handle OOB
+	v := (grid[i/8] >> (7 - i%8)) & 1
+
+	c.JSON(http.StatusOK, v)
+}
+
 func main() {
-	hub := newHub()
-	go hub.run()
+	router := gin.Default()
+	router.GET("/pixel", getPixel)
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router.Run(":8080")
 }
